ab03bf5a16d95db82b1403343352001b31c5adc1
RJ <rj@macklem.ca>
2024-03-23 17:02:37 -0400

    Create: Assignment4 pt 4 folder
    Created assignment 4 pt 4 folder (copy of pt3 folder as starting point per MDN)

---
+++ INFT1206-Assignment4-pt-4/main.js
@@ -0,0 +1,122 @@
+//	Name: Robert Macklem
+//  File: index.html
+//	Date: 23 March 2024
+//	Desc: Bouncing Balls MDN OOJS exercise html file
+
+//CONSTS
+//setup canvas
+const canvas = document.querySelector("canvas");
+
+//draw context
+const ctx = canvas.getContext("2d");
+
+//borders
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+//FUNCS
+// function to generate random number
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function to generate random color
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
+//SCRIPT
+//ball obj
+class Ball {
+  //Constructor
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+
+  //Draw method
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  //Update method (handles velocity changes/mvmt)
+  update() {
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+  
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+  
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+
+  //Collision handling
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+}
+
+//Balls array
+const balls = [];
+
+//While loop to populate it up to 25 balls
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size,
+  );
+
+  balls.push(ball);
+}
+
+//Add main loop
+function loop() {
+  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+//Call main loop
+loop();
